0990364e8f22718da257ae8fbc874c49020b63bb
braunfuss <asteinbe@gfz-potsdam.de>
2021-05-18 09:43:17 +0200

    licsar download by lat/lon

--- src/apps/weathertop_clients.py
+++ src/apps/weathertop_clients.py
@@ -3,6 +3,9 @@ import os
 import re
 import requests
 import urllib.request
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+import numpy as num
 
 op = os.path
 logging.basicConfig(level=logging.INFO)
@@ -20,34 +23,52 @@ def _download_file(url, outfile):
     return outfile
 
 
-def download_licsar_stack(tmin, tmax, track_number, destination="."):
-
+def search_tracks(tmin, tmax, lat, lon, destination, track_number):
     fp = urllib.request.urlopen("http://gws-access.jasmin.ac.uk/public/nceo_geohazards/LiCSAR_products/%s" % (track_number))
     page = fp.readlines()
-    for line in page[11:-4]:
-        line = line.decode("utf8")
-        idx = line.find("href=")
-        sub = line[idx+6:idx+23]
-        try:
-            fp = urllib.request.urlopen("http://gws-access.jasmin.ac.uk/public/nceo_geohazards/LiCSAR_products/%s/%s/interferograms/" % (track_number, sub))
+    try:
+        for line in page[11:-4]:
+            line = line.decode("utf8")
+            idx = line.find("href=")
+            sub = line[idx+6:idx+23]
+            fp = urllib.request.urlopen("http://gws-access.jasmin.ac.uk/public/nceo_geohazards/LiCSAR_products/%s/%s/metadata/%s-poly.txt" % (track_number, sub, sub))
             page = fp.readlines()
-            nr_lines = len(page)
+            coords = []
             for line in page:
                 line = line.decode("utf8")
-                idx = line.find("href=")
-                date1 = line[idx+6:idx+14]
-                date2 = line[idx+15:idx+23]
-                if date1 >= tmin and date2 <= tmax:
-                    unw_url = "http://gws-access.jasmin.ac.uk/public/nceo_geohazards/LiCSAR_products/%s/%s/interferograms/%s" % (track_number, sub, line[idx+6:idx+23])
-                    download_licsar(unw_url, destination=destination)
-        except:
-            pass
+                coord = line.split()
+                coords.append(num.array([float(coord[0]), float(coord[1])]))
+            point = Point(float(lat), float(lon))
+            polygon = Polygon(coords)
+            if polygon.contains(point) is True:
+                fp = urllib.request.urlopen("http://gws-access.jasmin.ac.uk/public/nceo_geohazards/LiCSAR_products/%s/%s/interferograms" % (track_number, sub))
+                page = fp.readlines()
+                nr_lines = len(page)
+                for line in page:
+                    line = line.decode("utf8")
+                    idx_sub = line.find('href="2')
+                    if idx_sub is not -1:
+                        date1 = int(line[idx_sub+6:idx_sub+14])
+                        date2 = int(line[idx_sub+15:idx_sub+23])
+                        if date1 >= int(tmin) and date2 <= int(tmax):
+                            unw_url = "http://gws-access.jasmin.ac.uk/public/nceo_geohazards/LiCSAR_products/%s/%s/interferograms/%s" % (track_number, sub, line[idx_sub+6:idx_sub+23])
+                            download_licsar(unw_url+".unw.tif", destination=destination)
+    except:
+        pass
+
+
+def download_licsar_stack(tmin, tmax, lat, lon, destination="."):
+    import multiprocessing as mp
+    pool = mp.Pool(mp.cpu_count())
+    tracks = range(1, 175)
+    results = [pool.apply(search_tracks, args=(tmin, tmax, lat, lon, destination, track_number)) for track_number in tracks]
+    pool.close()
 
 
 def download_licsar(unw_url, destination='.'):
     if not unw_url.endswith('.unw.tif'):
         raise ValueError('%s does not end with .unw.tif!' % unw_url)
-
+    print(unw_url)
     scene_name = op.basename(unw_url)
     url_dir = op.dirname(unw_url)
     product_name = op.basename(unw_url)
